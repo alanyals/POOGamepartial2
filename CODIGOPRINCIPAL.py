be5755d8219d5437d9da5aa3572f4ccc8a8b6580
arilany <ariadna00stroker@gmail.com>
2024-03-10 22:16:53 -0600

    Mejora de anahi cuatro

--- CODIGOPRINCIPAL.py
+++ CODIGOPRINCIPAL.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 class Paddle:
     def __init__(self, x, y, name):
@@ -14,7 +15,8 @@ class Paddle:
         self.__paddle.shapesize(stretch_wid=5, stretch_len=1)  # Tamaño de la paleta.
         self.__paddle.penup()  # Levanta el lápiz para evitar que se dibuje la línea.
         self.__paddle.goto(self.__x, self.__y)  # Posiciona la paleta en las coordenadas especificadas.
-        self.__name = name [:5] #limita el nombre personalizado a 5 caracteres.
+        self.__name = self.__get_valid_name(name)  # Limita el nombre personalizado y realiza la validación.
+
     def move_up(self):
         # Método para mover la paleta hacia arriba.
         self.__y += self.__speed if self.__y + self.__speed <= 290 else 0  # Limita el movimiento para no salir de la pantalla.
@@ -29,10 +31,19 @@ class Paddle:
         # Método para obtener la posición actual de la paleta.
         return self.__x, self.__y
 
-    def get_name (self): #metodo para obtener el nombre del jugador.
+    def get_name(self):  # Método para obtener el nombre del jugador.
         return self.__name
 
-
+    def __get_valid_name(self, name):
+        # Método privado para obtener un nombre válido.
+        while True:
+            if any(char.isdigit() for char in name):
+                print("No se aceptan números en el nombre. Ingrese un nombre válido.")
+            elif len(name) > 5:
+                print("El nombre excede el límite de 5 caracteres. Ingrese un nombre válido.")
+            else:
+                return name
+            name = input("Ingrese el nombre nuevamente (máximo 5 caracteres): ")
 
 class Ball:
     def __init__(self):
@@ -62,43 +73,59 @@ class PongGame:
         # Inicializa el juego Pong.
         self.__marcadorA = 0  # Puntuación del jugador A.
         self.__marcadorB = 0  # Puntuación del jugador B.
-        self.__tiempo = 0 #Es la variable en donde se guarda el tiempo
+        self.__tiempo = 0  # Es la variable en donde se guarda el tiempo
         self.__wn = turtle.Screen()  # Crea una pantalla de Turtle.
         self.__wn.title("Pong by Mundo Python")  # Título de la ventana.
         self.__wn.bgcolor("black")  # Color de fondo de la ventana.
         self.__wn.setup(width=800, height=600)  # Tamaño de la ventana.
         self.__wn.tracer(0)  # Desactiva las actualizaciones automáticas.
-        jugadorA_nombre = input("Nombre del jugador A (maximo 5 caracteres): [:5]")
-        jugadorB_nombre = input("Nombre del jugador A (maximo 5 caracteres): [:5]")
+        jugadorA_nombre = input("Nombre del jugador A (máximo 5 caracteres): ")
+        jugadorB_nombre = input("Nombre del jugador B (máximo 5 caracteres): ")
         self.__jugadorA = Paddle(-350, 0, jugadorA_nombre)  # Crea la paleta del jugador A con nombre personalizado.
-        self.__jugadorB = Paddle(350, 0,jugadorB_nombre)   # Crea la paleta del jugador B con nombre personalizado.
+        self.__jugadorB = Paddle(350, 0, jugadorB_nombre)   # Crea la paleta del jugador B con nombre personalizado.
         self.__pelota = Ball()  # Crea la pelota.
-        self.__pen = turtle.Turtle()  # Crea un objeto Turtle para dibujar el marcador.
-        self.__setup_pen()  # Configura el objeto Turtle para el marcador.
+        self.__pen_score = turtle.Turtle()  # Crea un objeto Turtle para dibujar el marcador.
+        self.__pen_timer = turtle.Turtle()  # Crea un objeto Turtle para dibujar el cronómetro.
+        self.__game_over_flag = False  # Variable para controlar el estado del juego.
+        self.__setup_pen()  # Configura los objetos Turtle para el marcador y el cronómetro.
 
     def __setup_pen(self):
         # Configura el marcador en la pantalla.
-        self.__pen.speed(0)  # Configura la velocidad de dibujo del marcador.
-        self.__pen.color("white")  # Color del marcador.
-        self.__pen.penup()  # Levanta el lápiz para no dibujar al moverse.
-        self.__pen.hideturtle()  # Oculta el cursor de Turtle.
-        self.__pen.goto(0, 260)  # Posiciona el marcador en la parte superior de la pantalla.
+        self.__pen_score.speed(0)  # Configura la velocidad de dibujo del marcador.
+        self.__pen_score.color("white")  # Color del marcador.
+        self.__pen_score.penup()  # Levanta el lápiz para no dibujar al moverse.
+        self.__pen_score.hideturtle()  # Oculta el cursor de Turtle.
         self.__update_score()  # Actualiza el marcador con la puntuación inicial.
-        self.__cronometro()#Actualiza el metodo del cronometro
+
+        # Configura el cronómetro en la pantalla.
+        self.__pen_timer.speed(0)  # Configura la velocidad de dibujo del cronómetro.
+        self.__pen_timer.color("white")  # Color del cronómetro.
+        self.__pen_timer.penup()  # Levanta el lápiz para no dibujar al moverse.
+        self.__pen_timer.hideturtle()  # Oculta el cursor de Turtle.
+        self.__update_timer()  # Actualiza el cronómetro con el tiempo inicial.
 
     def __update_score(self):
         # Actualiza el marcador con la puntuación actual de los jugadores.
-        self.__pen.clear()  # Borra el contenido anterior del marcador.
-        self.__pen.write(f"Jugador A: {self.__marcadorA}     Jugador B: {self.__marcadorB}", align="right", font=("Courier", 15, "normal"))  # Escribe la puntuación.
+        self.__pen_score.clear()  # Borra el contenido anterior del marcador.
+        self.__pen_score.goto(0, 230)  # Posiciona el marcador en la parte superior de la pantalla.
+        self.__pen_score.write(f"{self.__jugadorA.get_name()}: {self.__marcadorA}     {self.__jugadorB.get_name()}: {self.__marcadorB}", align="center", font=("Courier", 15, "normal"))  # Escribe la puntuación.
+
+    def __update_timer(self):
+        # Actualiza el cronómetro con el tiempo transcurrido.
+        self.__pen_timer.clear()  # Borra el contenido anterior del cronómetro.
+        self.__pen_timer.goto(-390, -290)  # Posiciona el cronómetro en la esquina inferior izquierda.
+        self.__pen_timer.write(f"Cronómetro: {self.__tiempo:.1f}", align="left", font=("Courier", 10, "normal"))
 
     def __check_collision(self):
         # Verifica las colisiones de la pelota con las paletas y los bordes de la pantalla.
         x, y = self.__pelota.get_position()  # Obtiene la posición actual de la pelota.
         if y > 290 or y < -290:
             self.__pelota._Ball__speed_y *= -1  # Invierte la velocidad de la pelota en el eje y al colisionar con los bordes superior o inferior.
-        if (-340 <= x <= -330) and (self.__jugadorA.get_position()[1] - 50 < y < self.__jugadorA.get_position()[1] + 50):
+        if (-340 <= x <= -330) and (
+                self.__jugadorA.get_position()[1] - 50 < y < self.__jugadorA.get_position()[1] + 50):
             self.__pelota._Ball__speed_x *= -1  # Invierte la velocidad de la pelota en el eje x al colisionar con la paleta del jugador A.
-        elif (340 >= x >= 330) and (self.__jugadorB.get_position()[1] - 50 < y < self.__jugadorB.get_position()[1] + 50):
+        elif (340 >= x >= 330) and (
+                self.__jugadorB.get_position()[1] - 50 < y < self.__jugadorB.get_position()[1] + 50):
             self.__pelota._Ball__speed_x *= -1  # Invierte la velocidad de la pelota en el eje x al colisionar con la paleta del jugador B.
         if x > 390:
             # Si la pelota sale por la derecha, incrementa la puntuación del jugador A y reinicia la posición de la pelota.
@@ -115,38 +142,47 @@ class PongGame:
             self.__marcadorB += 1
             self.__update_score()  # Actualiza el marcador con la nueva puntuación.
 
-    def __cronometro(self):
-        #metodo del cronometro
-        self.__tiempo +=0.1
-        self.__pen.write(f"Cronometro: {self.__tiempo:.1f}", align="left", font=("Courier",15 ,"normal"))
-        self.__wn.ontimer(self.__cronometro,100)
-        
-        
+    def __countdown(self):
+        # Temporizador de cuenta regresiva antes de iniciar el juego.
+        for i in range(10, 0, -1):# Itera desde 3 hasta 1 (exclusivo) en orden descendente.
+            self.__pen_timer.clear() # Borra el contenido anterior del temporizador.
+            self.__pen_timer.goto(0, 0) #se utiliza para asegurarse de que los números de la cuenta regresiva se escriban en el centro de la pantalla
+            self.__pen_timer.write(f"{i}", align="center", font=("Courier", 25, "normal"))# Escribe el número de cuenta regresiva.
+            self.__wn.update() #se utiliza para actualizar la pantalla de Turtle después de cada cuenta regresiva
+            time.sleep(1)  # Espera un segundo.
+        self.__pen_timer.clear() # Borra el contenido del temporizador para dejar la pantalla limpia antes de iniciar el juego.
+
     def play(self):
         # Inicia el juego.
+        self.__countdown()  # Ejecuta el temporizador de cuenta regresiva antes de iniciar el juego.
         self.__wn.listen()  # Habilita la escucha de eventos del teclado.
         self.__wn.onkeypress(self.__jugadorA.move_up, "w")  # Configura la tecla 'w' para que mueva hacia arriba la paleta del jugador A.
         self.__wn.onkeypress(self.__jugadorA.move_down, "s")  # Configura la tecla 's' para que mueva hacia abajo la paleta del jugador A.
-        self.__wn.onkeypress(self.__jugadorB.move_up, "Up")   # Configura la flecha arriba para que mueva hacia arriba la paleta del jugador B.
+        self.__wn.onkeypress(self.__jugadorB.move_up, "Up")  # Configura la flecha arriba para que mueva hacia arriba la paleta del jugador B.
         self.__wn.onkeypress(self.__jugadorB.move_down, "Down")  # Configura la flecha abajo para que mueva hacia abajo la paleta del jugador B.
-        
+
+        start_time = time.time()  # Registra el tiempo de inicio del juego.
+
         while True:
             self.__wn.update()  # Actualiza la pantalla.
             self.__pelota.move()  # Mueve la pelota.
             self.__check_collision()  # Verifica las colisiones.
-            if self.__marcadorA == 3 or self.__marcadorB == 3:
+            self.__tiempo = time.time() - start_time  # Calcula el tiempo transcurrido desde el inicio del juego.
+            self.__update_timer()  # Actualiza el cronómetro con el tiempo transcurrido.
+            if self.__marcadorA == 20 or self.__marcadorB == 20:
                 self.__game_over()
                 break
+        turtle.done()
 
     def __game_over(self):
-        # Muestra "Fin del juego" en el centro de la pantalla.
-        self.__pen.clear()
-        self.__pen.goto(0, 0)
-        self.__pen.write("Fin del juego", align="center", font=("Courier", 25, "normal"))
+        self.__game_over_flag = True
+        self.__pen_score.clear()
+        self.__pen_score.goto(0, 0)
+        ganador = "Jugador A" if self.__marcadorA == 20 else "Jugador B"
+        self.__pen_score.write(f"Fin del juego\nGanador: {ganador}", align="center", font=("Courier", 25, "normal"))
+        self.__wn.update()
 
 if __name__ == "__main__":
     # Ejecuta el juego.
     game = PongGame()  # Crea una instancia del juego.
     game.play()  # Inicia el juego.
-
-
